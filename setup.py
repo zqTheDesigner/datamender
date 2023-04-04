bd7a90c097c0f921aa9e0b7dabae52a7e30ff7a4
zqthedesigner <zqthedesigner@gmail.com>
2023-04-04 12:26:21 +0800

    u

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='DataMender',
+      version='0.0.0',
+      description='Data process helper and util functions',
+      author='zqthedesigner',
+      author_email='zqthedesigner@gmail.com',
+      url='',
+      packages=[],
+     )
