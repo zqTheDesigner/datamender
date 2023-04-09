39f8974187ee1744e64a2b7d9e497edc1c38de07
zqthedesigner <zqthedesigner@gmail.com>
2023-04-10 01:33:48 +0800

    u

--- datamender/datasets/timeseries_products.py
+++ datamender/datasets/timeseries_products.py
@@ -11,6 +11,5 @@ def load_data():
         The timeseries products dataset.
     """
     return pd.read_csv(
-        os.path.join(os.path.dirname(__file__),
-                     'data/timeseries_products.csv'),
+        './data/timeseries_products.csv',
     )
